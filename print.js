baf5e518095a2179cbd909151ecfaecdc0dcf697
Wilcem <wildan00muzaki@gmail.com>
2024-06-07 14:30:23 +0700

    first commit

---
+++ print.js
@@ -0,0 +1,12 @@
+function printDiv(divID) {
+  //  var divElements = document.getElementById(divID).innerHTML;
+  // var oldPage = document.body.innerHTML;
+
+  //document.body.innerHTML =
+  //  "<html><head><title></title></head><body>" +
+  //divElements + "</body>";
+
+  window.print();
+
+  //document.body.innerHTML = oldPage;
+}
